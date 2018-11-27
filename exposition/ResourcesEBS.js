48f9417d5d0dd321633ad1a0a2f5642aadfb2b34
jacjtito <34219294+jacjtito@users.noreply.github.com>
2018-11-27 14:58:58 +0100

    logger management and basic auth (#2)

--- exposition/ResourcesEBS.js
+++ exposition/ResourcesEBS.js
@@ -1,12 +1,18 @@
 import express from 'express'
 import * as resourcesLBS from '../business/ResourcesLBS'
 
+// Import *again* WINSTON => Ugly, what is the good way to do this?
+import * as winston from "../config/winston"
+const logger = winston.setLogger()
+
 export const  getRouter = () => {
   let resourcesRouter = express.Router()
 
   resourcesRouter.get('/', async (req, res) => {
     try {
-      res.send(await resourcesLBS.findResources())
+		logger.info("GET Request received over /")
+		res.json({"success":true})
+      //res.send(await resourcesLBS.findResources())
     } catch (error) {
       console.log('An error occured', error)
       res.status(500).send('An error occured')
@@ -24,7 +30,10 @@ export const  getRouter = () => {
 
   resourcesRouter.post('/', async (req, res) => {
     try {
-      res.send(await resourcesLBS.createResource(req.body))
+		
+			logger.info("POST Request received over /: " + JSON.stringify(req.body))
+			res.send(JSON.stringify(req.body))
+		  //res.send(await resourcesLBS.createResource(req.body))
     } catch (error) {
       console.log('An error occured', error)
       res.status(500).send('An error occured')
--- server.js
+++ server.js
@@ -1,18 +1,32 @@
+// For logging purpose
+import * as winston from "./config/winston"
+const logger = winston.setLogger()
+
+
 import express from 'express'
 import bodyParser from 'body-parser'
-import * as resourcesEBS from './exposition/ResourcesEBS'
+import * as resourcesEBS from "/home/pi/hivent-resources-api/exposition/ResourcesEBS"
 import * as actuatorEBS from './exposition/ActuatorEBS'
+import basicAuth from 'express-basic-auth'
 
+
+
+const basicAuthConfig = { users: { 'test': 'test' }}
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8084
+
+
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+  logger.info(`Hivent Resources service started on http://localhost:${port}`)
 })
+
+// pour exposer en basic Auth
+app.use(basicAuth(basicAuthConfig))
 
 // Pour récupérer body des requêtes
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/resources', resourcesEBS.getRouter())
 app.use('/actuator', actuatorEBS.getRouter())
