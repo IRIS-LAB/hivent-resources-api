f79495ec201ef30f4c1abc091bb14b9f17738a38
Patrick Tarnaud <PTARNAUD@groupement.systeme-u.fr>
2019-01-04 17:34:45 +0100

    unit test on ResourcesLBS checkRommResourceBE

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 // For logging purpose
-import * as winston from './config/winston'
-import express from 'express'
-import bodyParser from 'body-parser'
-import * as resourcesEBS from './src/exposition/ResourcesEBS'
-import * as actuatorEBS from './src/exposition/ActuatorEBS'
+import * as winston from './config/winston';
+import express from 'express';
+import bodyParser from 'body-parser';
+import * as resourcesEBS from './src/exposition/ResourcesEBS';
+import * as actuatorEBS from './src/exposition/ActuatorEBS';
 // import basicAuth from 'express-basic-auth'
 
+// TESTS
+// import * as ResourcesLBSTest from './src/business/ResourcesLBS.test';
+
 //const basicAuthConfig = { users: { test: 'test' } }
-const logger = winston.logger
-const app = express()
-const port = process.env.PORT || 8080
+const logger = winston.logger;
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-	logger.info(`Hivent Resources API started on http://localhost:${port}`)
-})
+	logger.info(`Hivent Resources API started on http://localhost:${port}`);
+});
 
 // pour exposer en basic Auth
 // app.use(basicAuth(basicAuthConfig))
 
 // Pour récupérer body des requêtes
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/resources', resourcesEBS.getRouter())
-app.use('/actuator', actuatorEBS.getRouter())
+app.use('/resources', resourcesEBS.getRouter());
+app.use('/actuator', actuatorEBS.getRouter());
--- src/business/ResourcesLBS.js
+++ src/business/ResourcesLBS.js
@@ -1,79 +1,103 @@
-import * as resourcesDAO from '../data/ResourcesDAO'
+import * as resourcesDAO from '../data/ResourcesDAO';
 import { ResourceTypeEnum } from '../objects/business/be/ResourceTypeEnum';
-import {BusinessException, ErrorDO} from 'iris-elements'
+import { BusinessException, ErrorDO } from 'iris-elements';
 
-const MAX_NAME_LENGTH = 100
-const MAX_SEATS_NUMBER = 1000
+export const MAX_NAME_LENGTH = 100;
+export const MAX_SEATS_NUMBER = 1000;
 
-const checkRoomResourceBE = resourceBE => {
-	let errors = []
+export const checkRoomResourceBE = resourceBE => {
+	let errors = [];
 	// name
 	if (!resourceBE.name) {
-		errors.push(new ErrorDO('name','resource.name.required','Le nom est obligatoire.'))
+		errors.push(new ErrorDO('name', 'resource.name.required', 'Le nom est obligatoire.'));
 	} else {
 		if (resourceBE.name.length > MAX_NAME_LENGTH) {
-			errors.push(new ErrorDO('name','resource.name.length','La longueur du nom ne doit pas dépasser 100 caractères.'))
+			errors.push(
+				new ErrorDO('name', 'resource.name.length', 'La longueur du nom ne doit pas dépasser 100 caractères.')
+			);
 		}
 	}
 	// type
 	if (!resourceBE.type) {
-		errors.push(new ErrorDO('type','resource.type.required','Le type est obligatoire.'))
+		errors.push(new ErrorDO('type', 'resource.type.required', 'Le type est obligatoire.'));
 	} else {
-		if (resourceBE.type in ResourceTypeEnum === false) {
-			errors.push(new ErrorDO('type','resource.type.required',`Le type ${resourceBE.type} n'est pas connu.`))
+		if (resourceBE.type !== ResourceTypeEnum.ROOM) {
+			errors.push(new ErrorDO('type', 'resource.type.invalid', `Le type ${resourceBE.type} doit être ROOM.`));
 		}
 	}
 	// mail
 	if (!resourceBE.mail) {
-		errors.push(new ErrorDO('mail','resource.mail.required','L\'email est obligatoire.'))
+		errors.push(new ErrorDO('mail', 'resource.mail.required', "L'email est obligatoire."));
 	} else {
-		let regex = /^(\s*|[a-zA-Z0-9._-]+@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,4})$/
+		let regex = /^(\s*|[a-zA-Z0-9._-]+@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,4})$/;
 		if (!regex.test(resourceBE.mail)) {
-			errors.push(new ErrorDO('mail','resource.mail.invalid','L\'email est invalide.'))
+			errors.push(new ErrorDO('mail', 'resource.mail.invalid', "L'email est invalide."));
 		}
 	}
 	// nbSeatsAvailable
-	if (!resourceBE.nbSeatsAvailable) {
-		errors.push(new ErrorDO('nbSeatsAvailable','resource.nbSeatsAvailable.required','Le nombre de places est obligatoire.'))
+	if (!resourceBE.nbSeatsAvailable && resourceBE.nbSeatsAvailable !== 0) {
+		errors.push(
+			new ErrorDO('nbSeatsAvailable', 'resource.nbSeatsAvailable.required', 'Le nombre de places est obligatoire.')
+		);
 	} else {
-		if (!Number.isInteger(resourceBE.nbSeatsAvailable)) {
-			errors.push(new ErrorDO('nbSeatsAvailable','resource.nbSeatsAvailable.invalid','Le nombre de places doit être un entier.'))
+		if (!Number.isInteger(resourceBE.nbSeatsAvailable) || resourceBE.nbSeatsAvailable < 1) {
+			errors.push(
+				new ErrorDO('nbSeatsAvailable', 'resource.nbSeatsAvailable.invalid', 'Le nombre de places doit être un entier.')
+			);
 		} else {
 			if (resourceBE.nbSeatsAvailable > MAX_SEATS_NUMBER) {
-				errors.push(new ErrorDO('nbSeatsAvailable','resource.nbSeatsAvailable.max','Le nombre de places doit inférieur à ' + MAX_SEATS_NUMBER))
+				errors.push(
+					new ErrorDO(
+						'nbSeatsAvailable',
+						'resource.nbSeatsAvailable.max',
+						'Le nombre de places doit inférieur à ' + MAX_SEATS_NUMBER
+					)
+				);
 			}
 		}
 	}
 	// projectorAvailable
 	if (typeof resourceBE.projectorAvailable != 'boolean') {
-		errors.push(new ErrorDO('projectorAvailable','resource.projectorAvailable.invalid','L\'indicateur de disponibilité de projecteur est incorrect.'))
+		errors.push(
+			new ErrorDO(
+				'projectorAvailable',
+				'resource.projectorAvailable.invalid',
+				"L'indicateur de disponibilité de projecteur est incorrect."
+			)
+		);
 	}
 	// chromeboxAvailable
 	if (typeof resourceBE.chromeboxAvailable != 'boolean') {
-		errors.push(new ErrorDO('chromeboxAvailable','resource.chromeboxAvailable.invalid','L\'indicateur de disponibilité de chromebox est incorrect.'))
+		errors.push(
+			new ErrorDO(
+				'chromeboxAvailable',
+				'resource.chromeboxAvailable.invalid',
+				"L'indicateur de disponibilité de chromebox est incorrect."
+			)
+		);
 	}
 
-	return errors
-}
+	return errors;
+};
 
 export const findResources = async () => {
-	return await resourcesDAO.findResources()
-}
+	return await resourcesDAO.findResources();
+};
 
 export const getResource = async resourceId => {
-	return await resourcesDAO.getResource(resourceId)
-}
+	return await resourcesDAO.getResource(resourceId);
+};
 
 export const createResource = async resourceBE => {
-	console.log('ResourceLBS : ' + resourceBE)
-	let errors = checkRoomResourceBE(resourceBE)
+	console.log('ResourceLBS : ' + resourceBE);
+	let errors = checkRoomResourceBE(resourceBE);
 	if (errors.length > 0) {
-		throw new BusinessException(errors)
+		throw new BusinessException(errors);
 	}
-	return await resourcesDAO.createResource(resourceBE)
-}
+	return await resourcesDAO.createResource(resourceBE);
+};
 
-export const init = async ()  => {
-	console.log('ResourcesLBS init ')
-	return await resourcesDAO.init()
-}
+export const init = async () => {
+	console.log('ResourcesLBS init ');
+	return await resourcesDAO.init();
+};
